dbfb7b1788272e9a6e48da41bda8982899d3db3b
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2021-05-10 14:11:13 +0700

    add work

--- src/test.py
+++ src/test.py
@@ -1,5 +1,8 @@
 from openpyxl import load_workbook
 
+from src.consts import path_template_file, path_out_dir
+from src.logic import read_file, make_invoice_file
+
 
 def test_1():
     wb = load_workbook("../resources/template.xlsx")
@@ -13,6 +16,15 @@ def test_2():
         print(sheet_name)
 
 
+def test_3():
+    invoices = read_file("/home/vladimir/Проекты/Генератор файлов/Задание/Копия реестр 2021.xlsx")
+    for invoice in list(invoices)[2:]:
+        make_invoice_file(invoice, path_template_file, path_out_dir)
+
+
 if __name__ == '__main__':
-    # test_1()
+    test_1()
+    print()
     test_2()
+    print()
+    test_3()
